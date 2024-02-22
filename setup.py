1bef3fc3abfc1899d5d5daa1697677056e49dfd4
carlamasche <carla.oruemascheroni@crowdaronline.com>
2024-02-22 16:44:13 -0300

    "update"

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import setuptools
 setuptools.setup(
     name='adapter-pytest',
     version='1.0',
-    description='',
     url='#',
+    author='LTM',
     packages=setuptools.find_packages(),
+    zip_safe=False
 )
